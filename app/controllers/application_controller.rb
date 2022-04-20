66714f63ef221647f284f8525573ebc7a6622812
Gerren <g.ballard23@outlook.com>
2022-04-20 08:15:13 -0500

    commit tree 8d742c654ac58f6db1d86af13778aefe4840beba

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -23,4 +23,26 @@ class ApplicationController < Sinatra::Base
     baked_good.to_json
   end
 
+  post '/baked_goods' do
+    baked = BakedGood.create(
+      name: params[:name],
+      price: params[:price],
+      bakery_id: params[:bakery_id]
+    )
+    baked.to_json
+  end
+
+  patch '/bakeries/:id' do
+    baked = Bakery.find(params[:id])
+    baked.update(
+      name: params[:name]
+    )
+    baked.to_json
+
+  end
+
+  delete '/baked_goods/:id' do
+    baked = BakedGood.delete(params[:id])
+  end
+
 end
